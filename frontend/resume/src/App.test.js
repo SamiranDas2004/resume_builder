83859c07435e325802637e6da8c5f030a18d8637
Samiran_Das <samiran4209@gmail.com>
2024-11-28 22:50:28 +0530

    commit

--- frontend/resume/src/App.test.js
+++ frontend/resume/src/App.test.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Login from './auth/Login';
 
+
+
+
 test('renders welcome message', () => {
   render(<Login />);
   const welcomeElement = screen.getByText(/Login/i);
